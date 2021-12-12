41ddd13acb6736901c4cd1c2e4fee473f596d22d
Cade Ferguson <cdferguson4@gmail.com>
2021-12-12 17:23:23 -0600

    Trying to get site to work

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -7,6 +7,6 @@ app_name = 'MainApp'
 urlpatterns = [
     path('',views.index,name='index'),
     path('Pizzas', views.pizzas, name='pizzas'),
-
+    path('pizza/<int:pizza_id>/',views.pizzas,name='pizza')
 
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -12,3 +12,11 @@ def pizzas(request):
     context = {'pizzas':pizzas}
 
     return render(request, 'MainApp/pizzas.html',context)
+
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.toppings_set.order_by('-date_added')
+    comments = pizza.comment_set.order_by('-date_added')
+
+    context = {'pizza': pizza, 'toppings': toppings, 'comments': comments}
+    return render(request, 'pizzas/pizzas.html', context)
