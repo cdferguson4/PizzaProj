381b900d3803441739e00aa07bdc70a3eccdfede
Cade Ferguson <cdferguson4@gmail.com>
2021-12-12 16:09:29 -0600

    ADDING PAGES

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'MainApp/index.html')
+
+from .models import Pizza
+
+def pizzas(request):
+    pizzas = Pizza.objects.filter(owner=request.user).order_by('date_added')
+    context = {'pizzas':pizzas}
+
+    return render(request, 'MainApp/pizzas.html',context)
